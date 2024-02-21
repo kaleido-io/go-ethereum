e311d25f1592b595a1d4529dc51ac6136aaf6a0b
Galen Keene <galen.keene@kaleido.io>
2024-02-21 17:19:55 -0500

    update config file logs

--- cmd/geth/config.go
+++ cmd/geth/config.go
@@ -129,12 +129,13 @@ func makeConfigNode(ctx *cli.Context) (*node.Node, gethConfig) {
 	}
 
 	// Load config file.
-	log.Info(fmt.Sprintf("Checking for config file: %v", configFileFlag.Name))
+	log.Info(fmt.Sprintf("Checking for config file"))
 	if file := ctx.String(configFileFlag.Name); file != "" {
+		log.Info(fmt.Sprintf("Checking for config file to load: %v", file))
 		if err := loadConfig(file, &cfg); err != nil {
 			utils.Fatalf("%v", err)
 		}
-		log.Info(fmt.Sprintf("Loaded config file: %v", configFileFlag.Name))
+		log.Info(fmt.Sprintf("Loaded config file: %v", file))
 	} else {
 		log.Info(fmt.Sprintf("No config file specified"))
 	}
